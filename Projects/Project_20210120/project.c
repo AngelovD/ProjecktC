646356fe1c20de05c2e5a2835ac7b461f25ec787
VermilionPride <44975571+VermilionPride@users.noreply.github.com>
2021-01-20 23:03:38 +0200

    Solution

---
+++ Projects/Project_20210120/project.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int findDif(int a, int b){
+    
+    int res = a^b;
+    int counter=0;
+    unsigned char cCheck = 1;
+
+    while(res>0){
+        if(res&cCheck){
+            counter++;
+        }
+        res=res>>1;
+    }
+    return counter;
+}
+
+int main(void){
+
+    int x,y;
+
+    printf("First number= ");
+    scanf("%d",&x);
+    printf("Second number= ");
+    scanf("%d",&y);
+
+    int res = findDif(x,y);
+
+    printf("%d",res);
+
+
+    return 0;
+}
